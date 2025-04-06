ebfc7524f28feb2b392170d0a6df4cb6ac87a5a7
Lelisa Merga <lalygrace.merga@gmail.com>
2025-04-06 12:52:45 +0300

    Header and Footer

---
+++ netflix-clone/src/Components/Footer/Footer.jsx
@@ -0,0 +1,119 @@
+import Box from "@mui/material/Box";
+import { red } from "@mui/material/colors";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Link from "@mui/material/Link";
+import { Grid } from "@mui/material";
+
+export default function Footer() {
+  const netflixLinks = [
+    "Questions? Contact us.",
+    "FAQ",
+    "Help Center",
+    "Account",
+    "Media Center",
+    "Investor Relations",
+    "Jobs",
+    "Ways to Watch",
+    "Terms of Use",
+    "Privacy",
+    "Cookie Preferences",
+    "Corporate Information",
+    "Contact Us",
+    "Speed Test",
+    "Legal Notices",
+    "Only on Netflix",
+  ];
+
+  const listItems = netflixLinks.map((link) => (
+    <Link href="#" color="inherit">
+      {link}
+    </Link>
+  ));
+
+  return (
+    <>
+      <div
+        className=" bg-black pt-9 "
+        style={{ position: "fixed", bottom: "0", left: "0", width: "100%" }}
+      >
+        <div className="dark:bg-w-800  flex justify-center ">
+          <div className="max-w-xl mx-auto p-6 ">
+            <label
+              htmlFor="input-9"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-100 "
+              style={{ fontSize: "1rem" }}
+            >
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </label>
+            <div
+              className="flex items-center mt-1 bg-gray-100"
+              style={{ borderRadius: "20px" }}
+            >
+              <input
+                type="email"
+                id="input-9"
+                className="w-full h-10 px-3 text-sm text-gray-700 border border-r-0 rounded-r-none border-blue-500 focus:outline-none rounded shadow-sm"
+                placeholder="Your Email"
+                style={{ fontSize: "1rem" }}
+              />
+
+              <input
+                type="submit"
+                id="input-9"
+                value={"Get Started >"}
+                className="w-full h-10 px-3 text-sm text-gray-700 border border-r-0 rounded-r-none border-blue-500 focus:outline-none rounded shadow-sm"
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  fontWeight: "bolder",
+                  fontSize: "1.7rem",
+                  borderRadius: "20px",
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto w-full max-w-[1166px] px-4 xl:px-0">
+          <div className="flex flex-col justify-between sm:px-[18px] md:flex-row md:px-10">
+            {/* <div className="md:w-[316px]">
+              <p className="text-[18px] font-medium text-white"></p>
+              <h1 className="text-white font-extrabold">
+                <span className="text-rose-600">NETFLIX</span>
+              </h1>
+              <p />
+            </div> */}
+
+            <div style={{ color: "rgb(252, 133, 133)" }}>
+              <Grid
+                container
+                spacing={2}
+                sx={{
+                  justifyContent: "space-between",
+                  alignContent: "space-between",
+                }}
+              >
+                {listItems.map((item, index) => (
+                  <Grid item xs={6} lg={3} key={index}>
+                    {item}
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          </div>
+
+          <hr className="mt-[30px] text-white" />
+          <div className="flex items-center justify-center pb-8 pt-[9px] md:py-8">
+            <p className="text-[10px] font-normal text-white md:text-[12px]">
+              © Copyright 2024 All Rights Reserved by YOUR WEBSITES. PVT. LTD
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -0,0 +1,220 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const drawerWidth = 240;
+const navItems = ["Popular Now", "Plans", "Reasons to Join", "FAQ", "Sign In"];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}></Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center", color: "black" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        elevation={0}
+        sx={{
+          backgroundColor: "rgba(211, 202, 202, 0.3)", // Light translucent white
+          backdropFilter: "blur(10px)",
+          WebkitBackdropFilter: "blur(10px)", // For Safari support
+          boxShadow: "none",
+          borderBottom: "1px solid rgba(255, 255, 255, 0.2)",
+        }}
+        style={{ color: "black" }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <div
+              style={{
+                width: "9.25rem",
+                height: "2.5rem",
+                color: "rgb(229, 9, 20)",
+                fill: "currentColor",
+                display: "block",
+              }}
+            >
+              <svg
+                viewBox="0 0 111 30"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                aria-hidden="true"
+                role="img"
+                class="default-ltr-cache-17nnie8-StyledBrandLogo ev1dnif2"
+              >
+                <g>
+                  <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z"></path>
+                </g>
+              </svg>
+            </div>
+          </Typography>
+          <Button
+            variant="contained"
+            href="#contained-buttons"
+            style={{
+              color: "black",
+              backgroundColor: "white",
+              borderRadius: "20px",
+            }}
+          >
+            Sign in
+          </Button>
+        </Toolbar>
+        <Box
+          sx={{
+            display: {
+              xs: "none",
+              sm: "block",
+              marginTop: "",
+              textAlign: "center",
+              backgroundColor: "black",
+              borderRadius: "20px",
+            },
+            // width: "fit-content",
+            // // width: "auto",
+            // minWidth: "min-content",
+            // maxWidth: "max-content",
+
+            maxWidth: "fit-content",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          {navItems.map((item) => (
+            <Button
+              key={item}
+              sx={{
+                color: "white",
+              }}
+              style={{ fontSize: "1rem" }}
+            >
+              {item}
+            </Button>
+          ))}
+        </Box>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              color: "yellow",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
+          quibusdam, aliquam dolore excepturi quae. Distinctio enim at eligendi
+          perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod
+          itaque exercitationem, at ab sequi qui modi delectus quia corrupti
+          alias distinctio nostrum. Minima ex dolor modi inventore sapiente
+          necessitatibus aliquam fuga et. Sed numquam quibusdam at officia
+          sapiente porro maxime corrupti perspiciatis asperiores, exercitationem
+          eius nostrum consequuntur iure aliquam itaque, assumenda et! Quibusdam
+          temporibus beatae doloremque voluptatum doloribus soluta accusamus
+          porro reprehenderit eos inventore facere, fugit, molestiae ab officiis
+          illo voluptates recusandae. Vel dolor nobis eius, ratione atque
+          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis,
+          voluptatem! Cumque, eligendi unde aliquid minus quis sit debitis
+          obcaecati error, delectus quo eius exercitationem tempore. Delectus
+          sapiente, provident corporis dolorum quibusdam aut beatae repellendus
+          est labore quisquam praesentium repudiandae non vel laboriosam quo ab
+          perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
+          commodi nihil corrupti cum non fugiat praesentium doloremque
+          architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
+          molestiae dicta blanditiis est expedita eius debitis cupiditate porro
+          sed aspernatur quidem, repellat nihil quasi praesentium quia eos,
+          quibusdam provident. Incidunt tempore vel placeat voluptate iure
+          labore, repellendus beatae quia unde est aliquid dolor molestias
+          libero. Reiciendis similique exercitationem consequatur, nobis placeat
+          illo laudantium! Enim perferendis nulla soluta magni error, provident
+          repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
+          suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
+          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore
+          commodi reprehenderit rerum reiciendis! Quidem alias repudiandae eaque
+          eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
+          ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat
+          rem repellendus. Voluptates perspiciatis, in pariatur impedit, nam
+          facilis libero dolorem dolores sunt inventore perferendis, aut
+          sapiente modi nesciunt.
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
